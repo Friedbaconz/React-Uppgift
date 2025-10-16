a3e51028be657e6f3785be65b8d8a26459d85379
Oskar <Oskarwidell@outlook.com>
2025-10-16 14:22:50 +0200

    Initial commit base html work added

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <div className='wrapper'>
 
-        <header />
+        <Header />
         
         <main>
           <Routes>
