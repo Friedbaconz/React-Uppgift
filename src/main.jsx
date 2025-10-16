f0c2b8ade09bd516371fcf6ddf2a000fca77049d
Oskar <Oskarwidell@outlook.com>
2025-10-16 11:18:04 +0200

    first setup

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import NotFound from './pages/NotFound'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+
+  </StrictMode>,
+)
