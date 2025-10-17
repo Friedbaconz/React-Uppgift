4367e7c8563c2c6a574e30867421852ad875ffb5
Oskar <Oskarwidell@outlook.com>
2025-10-17 14:52:50 +0200

    Hero-Section almost finished

--- src/components/sections/Hero/Hero.jsx
+++ src/components/sections/Hero/Hero.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Hero.css'
+import Button from '../../elements/Button'
 
 function Hero() {
-  return (
-    <div>Hero</div>
-  )
+    return (
+        <section class="hero-section">
+            <div class="hero-bg">
+                <div class="hero-wrapper">
+                    <div class="hero-text-wrapper">
+                        <div class="hero-text-box">
+                            <p class="hero-text-info">Welcome to StorAid</p>
+                            <p class="hero-text-title">Space Simplified, Storage Perfect</p>
+                            <p class="hero-text-feed">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Aenean a sem magna. Etiam ac odio sit amet lorem fringilla sodales.
+                                Cras lobortis, libero at iaculis luctus, nisi ex pellentesque nisi, at euismod sem ipsum ac dolor.
+                            </p>
+                        </div>
+                        
+                        <div class="hero-button-box">
+                            <Button id="discover-more" text="Discover More" />
+                        </div>
+                    </div>
+                    <div class="hero-img-wrapper">
+                        <img class="hero-img" src="/HeroImg.svg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Hero
