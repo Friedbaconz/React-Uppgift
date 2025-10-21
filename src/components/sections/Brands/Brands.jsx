2616d5913127c200683c71d528d8edbc42b9a026
Oskar <Oskarwidell@outlook.com>
2025-10-21 16:26:45 +0200

    Brands section is almost done

---
+++ src/components/sections/Brands/Brands.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Brands.css'
+
+function Brands() {
+  return (
+    <section class="brands">
+        <div class="brands-bg">
+            <div class="brands-wrapper">
+                <img class="brands-logo" src="./Logoipsum1.svg" alt="" />
+                <img class="brands-logo" src="./Logoipsum2.svg" alt="" />
+                <img class="brands-logo" src="./Logoipsum3.svg" alt="" />
+                <img class="brands-logo" src="./Logoipsum4.svg" alt="" />
+                <img class="brands-logo" src="./Logoipsum5.svg" alt="" />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Brands
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/sections/Hero/Hero'
 import AboutUs from '../components/sections/AboutUs/AboutUs'
+import Brands from '../components/sections/Brands/Brands'
 
 const HomePage = () => {
   return (
     <section>
       <Hero/>
       <AboutUs/>
+      <Brands/>
     </section>
   )
 }
