886fe9fd5bf554448889e5f361703958d78522a8
Oskar <Oskarwidell@outlook.com>
2025-10-22 12:58:53 +0200

    Ourservice section done

--- src/components/sections/OurService/OurService.jsx
+++ src/components/sections/OurService/OurService.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './OurService.css'
+import Texttile from '../../elements/Texttile/Texttile'
 
 function OurService() {
   return (
@@ -7,10 +8,37 @@ function OurService() {
         <div className="ourservice-bg">
             <div className="ourservice-wrapper">
                 <div className="ourservice-content">
-                    <p className="ourservice-title"></p>
+                    <p className="ourservice-title">Our Service</p>
                     <div className="ourservice-info">
-                        <p className="ourservice-info-title"></p>
-                        <p className="ourservice-info-text"></p>
+                        <div className="ourservice-info-title-wrapper">
+                            <p className="ourservice-info-title">Specialized Storage for Every Special Item</p>
+                        </div>
+                        <div className="ourservice-info-text-wrapper">
+                           <p className="ourservice-info-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. 
+                            Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.
+                            </p> 
+                        </div>
+                    </div>
+                    <div className="ourservice-tiles">
+                        <div className="ourservice-texttilewrapper">
+                            <div className="ourservice-texttile-box">
+                                <Texttile title="Diverse Unit Sizes" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."/>
+                            </div>
+                            <div className="ourservice-texttile-box">
+                                <Texttile title="Moving Assistance" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."/>
+                            </div>
+                        </div>
+                        <div className="ourservice-tile-box"></div>
+                        <div className="ourservice-texttilewrapper">
+                            <div className="ourservice-texttile-box">
+                                <Texttile title="Vehicle Storage" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."/>
+                            </div>
+                            <div className="ourservice-texttile-box">
+                              <Texttile title="Top-Notch Security" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."/>  
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
