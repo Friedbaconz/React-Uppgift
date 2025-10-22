f1d9a75ba7af8e8e145f006d6597dba4ba38085e
Oskar <Oskarwidell@outlook.com>
2025-10-22 14:30:39 +0200

    Almost done with Testimonials

---
+++ src/components/sections/Testimonials/Testimonials.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+import './Testimonials.css'
+
+function Testimonials() {
+  return (
+    <section>
+        <div className="testimon-bg">
+            <div className="testimon-wrapper">
+                <div className="testimon-content">
+                    <div className="testimon-text-wrapper">
+                        <div className="Testimon-box">
+                            <p className="testimon-header">Testimonials</p>  
+                        </div>
+                        <div className="Testimon-box">
+                            <p className="testimon-title">See What Our Clients Have to Say</p>
+                        </div>
+                        <div className="Testimon-box">
+                            <p className="testimon-text-content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. 
+                            Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. 
+                            Phasellus eget elit gravida.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="testimon-comment-wrapper">
+
+                        <div className="testimon-comment">
+                            <div className="testimon-comment-box">
+                                <div className="testimon-rating">
+                                    <i class="bi bi-star-fill" ></i>
+                                    <i class="bi bi-star-fill" ></i>
+                                    <i class="bi bi-star-fill" ></i>
+                                    <i class="bi bi-star-fill" ></i>
+                                    <i class="bi bi-star-fill" ></i>
+                                </div>
+                                <div className="testimon-comment-text-wrapper">
+                                    <p className="testimon-comment-text">
+                                        Lorem ipsum dolor sit amet, consectetur adipis 
+                                        cing elit. Ut libero lectus, porta nec turpis sit 
+                                        amet, lobortis fringilla ante.
+                                    </p>
+                                </div>
+                                <div className="testimon-comment-profile-wrapper">
+                                    <div className="testimon-profile-img">
+                                        <img className="testimon-profile-img-source" src="" alt="" />
+                                    </div>
+                                    <div className="testimon-profile-text">
+                                        <div className="testimon-profile-text-box">
+                                            <p className="testimon-profile-name">Name</p>
+                                            <p className="testimon-profile-work">Customer</p> 
+                                        </div>
+                                    </div> 
+                                </div>
+                                <div className="testimon-tag-wrapper">
+                                    <img className="testimon-tag" src="./CommentTag.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="testimon-comment">
+                            <div className="testimon-comment-box">
+                                <div className="testimon-rating">
+                                    <i class="bi bi-star-fill" ></i>
+                                    <i class="bi bi-star-fill" ></i>
+                                    <i class="bi bi-star-fill" ></i>
+                                    <i class="bi bi-star-fill" ></i>
+                                    <i class="bi bi-star-fill" ></i>
+                                </div>
+                                <div className="testimon-comment-text-wrapper">
+                                    <p className="testimon-comment-text">
+                                        Lorem ipsum dolor sit amet, consectetur adipis 
+                                        cing elit. Ut libero lectus, porta nec turpis sit 
+                                        amet, lobortis fringilla ante.
+                                    </p>
+                                </div>
+                                <div className="testimon-comment-profile-wrapper">
+                                    <div className="testimon-profile-img">
+                                        <img className="testimon-profile-img-source" src="" alt="" />
+                                    </div>
+                                    <div className="testimon-profile-text">
+                                        <div className="testimon-profile-text-box">
+                                            <p className="testimon-profile-name">Name</p>
+                                            <p className="testimon-profile-work">Customer</p> 
+                                        </div>
+                                    </div> 
+                                </div>
+                                <div className="testimon-tag-wrapper">
+                                    <img className="testimon-tag" src="./CommentTag.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="testimon-comment">
+                            <div className="testimon-comment-box">
+                                <div className="testimon-rating">
+                                    <i class="bi bi-star-fill" ></i>
+                                    <i class="bi bi-star-fill" ></i>
+                                    <i class="bi bi-star-fill" ></i>
+                                    <i class="bi bi-star-fill" ></i>
+                                    <i class="bi bi-star-fill" ></i>
+                                </div>
+                                <div className="testimon-comment-text-wrapper">
+                                    <p className="testimon-comment-text">
+                                        Lorem ipsum dolor sit amet, consectetur adipis 
+                                        cing elit. Ut libero lectus, porta nec turpis sit 
+                                        amet, lobortis fringilla ante.
+                                    </p>
+                                </div>
+                                <div className="testimon-comment-profile-wrapper">
+                                    <div className="testimon-profile-img">
+                                        <img className="testimon-profile-img-source" src="" alt="" />
+                                    </div>
+                                    <div className="testimon-profile-text">
+                                        <div className="testimon-profile-text-box">
+                                            <p className="testimon-profile-name">Name</p>
+                                            <p className="testimon-profile-work">Customer</p> 
+                                        </div>
+                                    </div> 
+                                </div>
+                                <div className="testimon-tag-wrapper">
+                                    <img className="testimon-tag" src="./CommentTag.svg" alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Testimonials
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/sections/Hero/Hero'
 import AboutUs from '../components/sections/AboutUs/AboutUs'
 import Brands from '../components/sections/Brands/Brands'
 import OurService from '../components/sections/OurService/OurService'
+import Testimonials from '../components/sections/Testimonials/Testimonials'
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
       <AboutUs/>
       <Brands/>
       <OurService/>
+      <Testimonials/>
     </section>
   )
 }
