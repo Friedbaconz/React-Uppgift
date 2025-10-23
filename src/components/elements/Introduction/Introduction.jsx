9199c7d94b8bb4bad36b0e217b7817d6e96f2a6a
Oskar <Oskarwidell@outlook.com>
2025-10-23 13:05:30 +0200

    Done with introduction

--- src/components/elements/Introduction/Introduction.jsx
+++ src/components/elements/Introduction/Introduction.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import './Introduction.css'
 
-function Introduction({text, header, title}) {
+function Introduction({ text, header, title }) {
     return (
-        <div>
-            <p className="introduction-title">{header}</p>
+
+        <div className="introduction-wrapper">
+            <div className="introduction-header-wrapper">
+                <p className="introduction-title">{header}</p>
+            </div>
             <div className="introduction-info">
-                <div className="introduction-info-title-wrapper">
-                    <p className="introduction-info-title">{title}</p>
-                </div>
-                <div className="introduction-info-text-wrapper">
-                    <p className="introduction-info-text">
-                        {text}
-                    </p>
+                <div className="introduction-info-wrapper">
+                    <div className="introduction-info-title-wrapper">
+                        <p className="introduction-info-title">{title}</p>
+                    </div>
+                    <div className="introduction-info-text-wrapper">
+                        <p className="introduction-info-text">
+                            {text}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
--- src/components/sections/WhyChooseUs/WhyChooseUs.jsx
+++ src/components/sections/WhyChooseUs/WhyChooseUs.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './WhyChooseUs.css'
+import Introduction from '../../elements/Introduction/Introduction'
 
 function WhyChooseUs() {
   return (
     <section>
-        
+        <div className="whyus-bg">
+            <div className="whyus-wrapper">
+                <div className="whyus-content">
+                    <div className="whyus-introduction-wrapper">
+                        <Introduction header="Why Choose Us" title="Choose Us For Exceptional Storage Solutions" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida."/>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </section>
   )
