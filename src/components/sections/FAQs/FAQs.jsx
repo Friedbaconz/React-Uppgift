75ab0f12cf1b1eb47fc34fc3a80533c16e7242f9
Oskar <Oskarwidell@outlook.com>
2025-10-28 12:22:26 +0100

    Faq base is done

---
+++ src/components/sections/FAQs/FAQs.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './FAQs.css'
+import Introduction from '../../elements/Introduction/Introduction'
+
+function FAQs() {
+  return (
+    <section>
+        <div className="faq-bg">
+            <div className="faq-wrapper">
+                <div className="faq-content">
+                    <div className="faq-intro-wrapper">
+                        <p className="faq-intro-header">FAQs</p>
+                        <p className="faq-intro-title">Frequently Ask Questions</p>
+                        <p className="faq-into-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                    </div>
+
+                    <div className="faq-accordian-wrapper">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default FAQs
--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../components/sections/Banner/Banner'
 import GetInTouch from '../components/sections/GetInTouch/GetInTouch'
 import FindUs from '../components/sections/FindUs/FindUs'
+import FAQs from '../components/sections/FAQs/FAQs'
 
 function ContactUsPage() {
   return (
@@ -9,6 +10,7 @@ function ContactUsPage() {
         <Banner title="Contact Us" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."/>
         <GetInTouch/>
         <FindUs/>
+        <FAQs/>
     </section>
   )
 }
